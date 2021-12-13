72d68a46e1f30187bc0cb8c5e54aa5672422aa65
Charles Tan <charlestan@charles-macbook.rlab.cs.brown.edu>
2021-12-12 20:07:45 -0500

    Print loss every epoch, perform 10 epochs at 200 steps each

--- code/transfer.py
+++ code/transfer.py
@@ -13,18 +13,20 @@ def train_step(image, content_model, style_model):
     with tf.GradientTape() as tape:
         content_outputs = content_model(image)
         style_outputs = style_model(image)
-        style_loss = style_model.style_loss_calc(style_outputs)
-        content_loss = content_model.content_loss_calc(content_outputs)
-        loss = content_weight * content_loss + style_weight * style_loss
-        loss += total_variation_weight*tf.image.total_variation(image)
+        style_loss = style_weight * style_model.style_loss_calc(style_outputs)
+        content_loss = content_weight * content_model.content_loss_calc(content_outputs)
+        variation_loss = total_variation_weight*tf.image.total_variation(image)
+        loss =  content_loss +  style_loss + variation_loss
 
     grad = tape.gradient(loss, image)
     opt.apply_gradients([(grad, image)])
     image.assign(tf.clip_by_value(image, clip_value_min=0.0, clip_value_max=1.0))
+    return (content_loss, style_loss, variation_loss, loss)
+
 
 if __name__ == '__main__':
-    style_path = 'style_images/kokurikozaka049.jpg'
-    content_path = 'content_images/college_hill.png'
+    style_path = 'style_images/ponyo013.jpeg'
+    content_path = 'content_images/sayles.jpeg'
     style_path_name = re.split('\.|/', style_path)[-2]
     content_path_name = re.split('\.|/', content_path)[-2]
     style_image = utils.load_img(style_path)
@@ -53,18 +55,19 @@ if __name__ == '__main__':
     import time
     start = time.time()
 
-    epochs = 5
+    epochs = 10
     steps_per_epoch = 200
 
     step = 0
     for n in range(epochs):
         for m in range(steps_per_epoch):
             step += 1
-            train_step(image, content_model, style_model)
+            content_loss, style_loss, variation_loss, loss = train_step(image, content_model, style_model)
             print(".", end='', flush=True)
         display.clear_output(wait=True)
         utils.tensor_to_image(image, file_name=(save_dir + '/' + style_path_name + '_' + content_path_name + '_' + str(n) + ".jpg"))
         print("Train step: {}".format(step))
+        print("content_loss", content_loss, "style_loss", style_loss, "variation_loss", variation_loss, "total_loss", loss)
 
     end = time.time()
     print("Total time: {:.1f}".format(end-start))
