9119508445f73f0328425b057917e72e17e74cde
yuan-pu <77847503+yuan-pu@users.noreply.github.com>
2021-12-14 17:07:39 -0500

    yuan - trials with Resnet

--- code/transfer.py
+++ code/transfer.py
@@ -43,11 +43,20 @@ if __name__ == '__main__':
 
     # resnet50
     content_layers = ['conv5_block3_3_conv']
-    style_layers = ['conv1_conv', 
-                    'conv2_block1_1_conv',
-                    'conv3_block1_1_conv',
-                    'conv4_block1_1_conv',
-                    'conv5_block1_1_conv']
+    # style_layers = ['conv1_conv', 
+    #                 'conv2_block1_1_conv',
+    #                 'conv3_block1_1_conv',
+    #                 'conv4_block1_1_conv',
+    #                 'conv5_block1_1_conv']
+    style_layers = ['conv5_block1_1_conv',
+                    'conv5_block1_2_conv',
+                    'conv5_block1_3_conv',
+                    'conv5_block2_1_conv',
+                    'conv2_block2_2_conv',
+                    'conv5_block2_3_conv',
+                    'conv5_block3_1_conv',
+                    'conv5_block3_2_conv',
+                    'conv5_block3_3_conv']
 
     style_model = StyleModel(style_image, style_layers)
     content_model = ContentModel(content_image, content_layers)
@@ -74,7 +83,7 @@ if __name__ == '__main__':
             content_loss, style_loss, variation_loss, loss = train_step(image, content_model, style_model)
             print(".", end='', flush=True)
         display.clear_output(wait=True)
-        utils.tensor_to_image(image, file_name=(save_dir + '/' + style_path_name + '_' + content_path_name + '_' + str(n) + ".jpg"))
+        utils.tensor_to_image(image, file_name=(save_dir + '/' + style_path_name + '_' + content_path_name + '_' + 'resnet' + '_' + str(n) + ".jpg"))
         print("Train step: {}".format(step))
         print("content_loss", content_loss, "style_loss", style_loss, "variation_loss", variation_loss, "total_loss", loss)
 
